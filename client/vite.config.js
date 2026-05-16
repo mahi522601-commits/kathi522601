be5c78567006177f823512fbc0efebbb02f45bdc
mahi522601 <mahi522601@gmail.com>
2026-05-16 11:12:50 +0530

    fix loading spinner import

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePluginSitemap } from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginSitemap({
+      hostname: 'https://khyathicollections.com',
+    }),
+  ],
 
   server: {
     port: 5173,
